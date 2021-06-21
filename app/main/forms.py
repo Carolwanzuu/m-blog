75449e4e23e1ceeb092f74ea718cf2ec24210207
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-21 17:16:04 +0300

    creating a registration form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
-from . import main
-from flask_login import login_required
+# from . import main
+# from flask_login import login_required
 
-@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
-@login_required
-def new_review(id):
+# @main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+# @login_required
+# def new_review(id):
