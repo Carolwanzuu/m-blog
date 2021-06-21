8bbf2e6750303a8cc72a707b12c7877ba4ac893e
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-21 17:10:59 +0300

    importing UserMixin

--- app/models.py
+++ app/models.py
@@ -1,13 +1,19 @@
 from . import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash,check_password_hash
-
+from flask_login import UserMixin
 
 # class User:
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    image_path = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    blog = db.relationship('Blog', backref = 'users', lazy = 'dynamic')
+    comment = db.relationship('Comment', backref = 'users', lazy = 'dynamic')
     pass_secure = db.Column(db.String(255))
 
     @property
