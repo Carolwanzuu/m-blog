32c232b919778f218886b71698635f99d0acbf16
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-21 15:55:05 +0300

    installing and initializing flask-bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
 
 def create_app(config_name):
     app = Flask(__name__)
+
+    bootstrap.init_app(app)
 
     return app
