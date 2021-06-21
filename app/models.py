d50e3336a02c8db98212e434e9e3004e996671b0
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-21 16:56:32 +0300

    impoting hashing functionality in user model

--- app/models.py
+++ app/models.py
@@ -10,9 +10,22 @@ class User(db.Model):
     username = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
 
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
-        return f'User {self.username}'
+           return f'User {self.username}'
+
+        
 
 class Blog:
     __tablename = 'blog'
