b76356f336c11d9bd2f2a1d9a088270e71c87656
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-21 16:52:23 +0300

    adding a new column to users model

--- app/models.py
+++ app/models.py
@@ -1,11 +1,15 @@
 from . import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
+
 
 # class User:
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+
 
     def __repr__(self):
         return f'User {self.username}'
--- manage.py
+++ manage.py
@@ -2,14 +2,14 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Server
 
 from app import create_app, db
-from app.models import Blog, User
+from app.models import User, Blog
 
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server', Server)
 
-migrate = Migrate(app)
+migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
 @manager.shell
