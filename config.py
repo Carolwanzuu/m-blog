282b0ff0a0832fe17483b37ab84e954ba89a64e7
Carolwanzuu <carolkyalo01@gmail.com>
2021-06-22 11:14:48 +0300

    configuring uploads

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     # SECRET_KEY = ''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blog'
-    
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
 class ProdConfig(Config):
     pass
